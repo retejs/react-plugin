87f633ecdfb03e6a8c93cdedcc84684272c5bea3
Ni55aN <huccah@gmail.com>
2023-01-09 22:42:12 +0200

    customizable connection styles

--- src/next/presets/classic/components/Connection.tsx
+++ src/next/presets/classic/components/Connection.tsx
@@ -13,24 +13,27 @@ const Svg = styled.svg`
     height: 9999px;
 `
 
-const Path = styled.path`
+const Path = styled.path<{ styles?: (props: any) => any }>`
     fill: none;
     stroke-width: 5px;
     stroke: steelblue;
     pointer-events: auto;
+    ${props => props.styles && props.styles(props)}
 `
 
-export function Connection(props: { data: ClassicScheme['Connection'] & { isLoop?: boolean } }) {
+export function Connection(props: { data: ClassicScheme['Connection'] & { isLoop?: boolean }, styles?: () => any }) {
     const { start, end } = useConnection()
     const points = start && end && [start.x, start.y, end.x, end.y]
     const curvature = 0.3
 
     return points && (
         <Svg>
-            <Path d={props.data.isLoop
-                ? loopConnectionPath(points, curvature, 120)
-                : classicConnectionPath(points, curvature)
-            } />
+            <Path
+                styles={props.styles}
+                d={props.data.isLoop
+                    ? loopConnectionPath(points, curvature, 120)
+                    : classicConnectionPath(points, curvature)
+                } />
         </Svg>
     )
 }
