ed1e7516ba040431e6e81c993f2e04ac89a9803c
Ni55aN <huccah@gmail.com>
2023-02-01 22:11:15 +0200

    wip (for v18 only): render into nested container
    allow other elements to be added alongside

--- src/next/renderer.ts
+++ src/next/renderer.ts
@@ -2,29 +2,41 @@ import * as ReactDOM from 'react-dom'
 
 export type Renderer = { mount: ReactDOM.Renderer, unmount: (container: HTMLElement) => void }
 
-export function getRenderer(props?: { createRoot?: (container: Element | DocumentFragment) => any }): Renderer {
-    const roots = new Map<HTMLElement, any>()
+type CreateRoot = (container: Element | DocumentFragment) => any
+
+export function getRenderer(props?: { createRoot?: CreateRoot }): Renderer {
     const createRoot = props?.createRoot
-    const mount = createRoot ? ((
-        element: React.DOMElement<React.DOMAttributes<any>, any>,
-        container: HTMLElement
-    ) : Element => {
-        if (!roots.has(container) && container) roots.set(container, createRoot(container))
-        const root = roots.get(container)
 
-        return root.render(element)
-    }) as ReactDOM.Renderer : ReactDOM.render
-    const unmount = createRoot ? (container: HTMLElement) => {
-        const root = roots.get(container)
+    if (createRoot) {
+        const roots = new Map<HTMLElement, any>()
 
-        if (root) {
-            root.unmount()
-            roots.delete(container)
+        return {
+            mount: ((
+                element: React.DOMElement<React.DOMAttributes<any>, any>,
+                container: HTMLElement
+            ) : Element => {
+                if (container.children.length === 0) container.appendChild(document.createElement('div'))
+                const child = container.children.item(0)
+                if (!roots.has(container) && child) {
+                    roots.set(container, createRoot(child))
+                }
+                const root = roots.get(container)
+
+                return root.render(element)
+            }) as ReactDOM.Renderer,
+            unmount: (container: HTMLElement) => {
+                const root = roots.get(container)
+
+                if (root) {
+                    root.unmount()
+                    roots.delete(container)
+                }
+            }
         }
-    } : ReactDOM.unmountComponentAtNode
+    }
 
     return {
-        mount,
-        unmount
+        mount: ReactDOM.render,
+        unmount: ReactDOM.unmountComponentAtNode
     }
 }
